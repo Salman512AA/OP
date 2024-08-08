855f2e932ff6e661b27ddf794e6105592a3b02ef
Salman512AA <163514581+Salman512AA@users.noreply.github.com>
2024-08-08 11:07:48 +0500

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,10 @@
-const mongoose= require('mongoose');
-const mongoURI="mongodb://localhost:27017/Ali"
-const connectToMongo=()=>{
-    mongoose.connect(mongoURI).then(()=>console.log("Connected")).catch((e)=>console.log(e.message))
-
-} 
-module.exports=connectToMongo;  
+const mongoose = require("mongoose");
+const mongoURI =
+  "mongodb+srv://mern-vercel:likEeo6kFwwGTZR0@mern-vercal-2.fvno8.mongodb.net/?retryWrites=true&w=majority&appName=mern-vercal-2";
+const connectToMongo = () => {
+  mongoose
+    .connect(mongoURI)
+    .then(() => console.log("Connected"))
+    .catch((e) => console.log(e.message));
+};
+module.exports = connectToMongo;
